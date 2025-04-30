73814918a9269e0430539bab8bb61cc33344c1b6
syntaxris97 <jesraelabogado@gmail.com>
2025-05-01 04:12:04 +0800

    Delete my-website/js directory

--- my-website/js/home.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.search button').addEventListener('click', function() {
-    const query = document.querySelector('.search input').value;
-    // Add your search logic here
-    alert('Searching for: ' + query);
-});
