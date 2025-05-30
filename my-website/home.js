98eb700b678e4e541da09aa3cf1b48484cb8fc4c
syntaxris97 <jesraelabogado@gmail.com>
2025-05-30 16:36:10 +0800

    Update home.js

--- my-website/home.js
+++ my-website/home.js
@@ -1,21 +1,21 @@
 const scripts = [
   {
     id: "5",
-    title: "Rabaya V1 Script",
+    title: "WQeqd V1 Script",
     description: "BLOX FRUITS Script No Key UPDATE GRAVITY AUTO…",
     date: "2025-05-28",
     image: "image/scriptroblox2.png"
   },
   {
     id: "4",
-    title: "Rabaya v3 Script",
+    title: "Dwqe v3 Script",
     description: "BLOX FRUITS Script Pastebin 2025 UPDATE GRAVITY…",
     date: "2025-05-28",
     image: "image/scriptroblox1.png"
   },
   {
     id: "1",
-    title: "Rabaya V3 Script",
+    title: "Sdaw V3 Script",
     description: "BLOX FRUITS Script No Key AUTO FARM, PREHISTORIC…",
     date: "2025-05-28",
     image: "image/scriptroblox3.png"
