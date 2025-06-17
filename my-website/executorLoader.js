d87ed936f2dd83b277c5781b8eb85f0a12637b49
syntaxris97 <jesraelabogado@gmail.com>
2025-06-17 13:48:28 +0800

    Update executorLoader.js

--- my-website/executorLoader.js
+++ my-website/executorLoader.js
@@ -1,9 +1,9 @@
 const executors = [
   {
-    id: "6",
-    title: "DELTA V675 BEST",
+    id: "7",
+    title: "TRIGON V676",
     author: "reaperscripts…",
-    date: "2025-06-03",
+    date: "2025-06-17",
     image: "image/deltaexe.jpg",
     description: "DELTA Executor Mobile and Emulator (FREE)",
     steps: [
@@ -15,13 +15,31 @@ const executors = [
       "Paste the script",
       "Run and done"
     ],
-    downloadLink: "https://www.mediafire.com/file/4mmopck1m3qc7fs/Delta-2.675.715.apk/file"
+    downloadLink: "https://www.mediafire.com/file/jptm9s9jntqf2z8/Trigon+v2.676.apk/file?dkey=zf6ov0k7oae&r=1865"
+  },
+  {
+    id: "6",
+    title: "DELTA V675 BEST",
+    author: "reaperscripts…",
+    date: "2025-06-017",
+    image: "image/deltaexe.jpg",
+    description: "DELTA Executor Mobile and Emulator (FREE)",
+    steps: [
+      "Click DOWNLOAD button",
+      "Complete the task",
+      "Download file",
+      "Install file",
+      "Get Key",
+      "Paste the script",
+      "Run and done"
+    ],
+    downloadLink: "https://www.mediafire.com/file/qpuyhzivftmr53n/Delta-2.676.531.apk/filehttps://www.mediafire.com/file/qpuyhzivftmr53n/Delta-2.676.531.apk/file"
   },
   {
     id: "5",
     title: "VEGA X V674",
     author: "reaperscripts…",
-    date: "2025-06-03",
+    date: "2025-06-17",
     image: "image/vegaxexe.jpg",
     description: "VEGA X Executor Mobile and Emulator (FREE)",
     steps: [
@@ -33,13 +51,13 @@ const executors = [
       "Paste the script",
       "Run and done"
     ],
-    downloadLink: "https://www.mediafire.com/file/oii8wt2zkvkgr96/Vega-X+674.apk/file"
+    downloadLink: "https://www.mediafire.com/file/16rp1ufd1onhkrn/Vega-X+2.676.apk/file"
   },
   {
     id: "4",
     title: "CODEX V675",
     author: "reaperscripts…",
-    date: "2025-06-03",
+    date: "2025-06-17",
     image: "image/codexexe.jpg",
     description: "CODEX Executor Mobile and Emulator (FREE)",
     steps: [
@@ -51,13 +69,13 @@ const executors = [
       "Paste the script",
       "Run and done"
     ],
-    downloadLink: "https://www.mediafire.com/file/26y4geu9v3dk9ul/Codex_v2.675.apk/file"
+    downloadLink: "https://www.mediafire.com/file/fm2bgegjsxlill8/Codex+v2.676.apk/file"
   },
   {
     id: "1",
     title: "CRYPTIC V674",
     author: "reaperscripts…",
-    date: "2025-06-03",
+    date: "2025-06-17",
     image: "image/crypticexe.jpg",
     description: "CRYPTIC Executor Mobile and Emulator (FREE)",
     steps: [
@@ -69,13 +87,13 @@ const executors = [
       "Paste the script",
       "Run and done"
     ],
-    downloadLink: "https://www.mediafire.com/file/rruut5uxog8hf59/Cryptic+2.674.2789+APK.apk/file"
+    downloadLink: "https://www.mediafire.com/file/277ppy9ovei30wp/Cryptic+2.676.531+APK.apk/file"
   },
   {
     id: "2",
     title: "FLUXUS V673",
     author: "shower head…",
-    date: "2025-05-27",
+    date: "2025-06-09",
     image: "image/fluxusexe.jpg",
     description: "FLUXUS Executor Mobile and Emulator",
     steps: [
@@ -93,7 +111,7 @@ const executors = [
     id: "3",
     title: "KRNL V673",
     author: "valentinodev…",
-    date: "2025-5-22",
+    date: "2025-6-17",
     image: "image/krnlexe.jpg",
     description: "KRNL is a powerful mobile Roblox executor.",
     steps: [
@@ -103,7 +121,7 @@ const executors = [
       "Open app and inject",
       "Use scripts safely"
     ],
-    downloadLink: "https://www.mediafire.com/file/vfoum72vv4hoajv/krnl_release_2.673.710_2025.5.17_26.apk/file"
+    downloadLink: "https://www.mediafire.com/file/tos1qc50hc4dq3e/krnl_release_2.677.762_2025.6.15_32.apk/file"
   }
 ];
 
